5f6cc1823bc5d42ea79bcec68c182a9905277094
Muhammad Hassaan Khan <hk7866@yahoo.com>
2025-08-06 02:54:10 +0500

    Your commit message here

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,14 @@ import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import React from "react";
 import Navbar from "@/components/main/NavBar";
-import { Dancing_Script } from 'next/font/google';
 import Footer from "@/components/main/Footer";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Hassaan Khan",
   description: "Hassaan's Personal Portfolio",
-   icons: {
+  icons: {
     icon: '/final.png',
   },
 };
@@ -25,24 +23,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="icon/png" href="/final.png" />
-        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
-      </head>
-      <body
-        className={`${inter.className}  overflow-y-scroll overflow-x-hidden`}
-      >
+      <body className={`${inter.className} overflow-y-scroll overflow-x-hidden`}>
         <div className="bg-[#030014]">
-
           <StarsCanvas />
           <Navbar />
-          
-        {children}
-        <Footer />
+          {children}
+          <Footer />
         </div>
       </body>
-        
-        
     </html>
   );
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/final.png" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
