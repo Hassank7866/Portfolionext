4795bbe6f057f2deadba025cbcefb63ef17a70cd
Muhammad Hassaan Khan <hk7866@yahoo.com>
2025-08-06 03:10:26 +0500

    Your descriptive commit message here

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -18,7 +18,7 @@ const Skills = () => {
       style={{ transform: "scale(0.9" }}
     >
         <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        Technologies I've worked with
+        Technologies I have worked with
       </h1>
     <SkillText />
       
